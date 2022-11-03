084163820e9780c11360e3d4fbc77e855c27461f
temka1007 <munhtemuulen.kh2002@gmail.com>
2022-11-03 17:22:55 +0800

    js changes

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.getElementsByTagName('h1')[0];
+const p = document.getElementsByTagName('p')[0];
+const ul = document.getElementsByTagName('ul')[0];
+
+for (let element of p.childNodes) {
+    console.log(element);
+  }
